badab4d4a25b8348b8f7a9226da2cc9c799202ab
Hummel-Wang <SDwangfu@126.com>
2018-12-25 16:22:53 +0800

    modify namespace

---
+++ AutoResetEventApp/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoResetEventApp
+{
+    class Program
+    {
+        #region 通知单一线程
+        //若要将初始状态设置为终止，则为 true；若要将初始状态设置为非终止，则为 false
+        static AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() =>
+            {
+                {
+                    Console.WriteLine("客户1在排队等待付钱...");
+
+                    //客户1调用AutoResetEvent上的WaitOne来等待付钱通知
+                    autoResetEvent.WaitOne();
+                    Console.WriteLine("通知来了，客户1付钱");
+                }
+            });
+            t1.IsBackground = true;
+            t1.Start();
+
+            Pay();//发送通知 
+            Console.ReadKey();
+        }
+        static void Pay()
+        {
+            string tip = Console.ReadLine();
+            if (tip == "next")
+            {
+                autoResetEvent.Set();//收银员发送付钱通知，通过调用Set来通知客户付钱
+            }
+        }
+        #endregion
+
+        #region 通知多个线程
+        ////若要将初始状态设置为终止，则为 true；若要将初始状态设置为非终止，则为 false
+        //static AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        //static void Main(string[] args)
+        //{
+        //    Thread t1 = new Thread(() =>
+        //    {
+        //        {
+        //            Console.WriteLine("客户1在排队等待付钱...");
+
+        //            //客户1调用AutoResetEvent上的WaitOne来等待付钱通知
+        //            autoResetEvent.WaitOne();
+        //            Console.WriteLine("通知来了，客户1付钱");
+        //            autoResetEvent.Set(); //AutoResetEvent.Set()只能每次通知一个线程，此处若不set便只会执行t1
+        //        }
+        //    });
+        //    t1.IsBackground = true;
+        //    t1.Start();
+
+        //    Thread t2 = new Thread(() =>
+        //    {
+        //        {
+        //            Console.WriteLine("客户2在排队等待付钱...");
+
+        //            //客户2调用AutoResetEvent上的WaitOne来等待付钱通知
+        //            autoResetEvent.WaitOne();
+        //            Console.WriteLine("通知来了，客户2付钱");
+        //        }
+        //    });
+        //    t2.IsBackground = true;
+        //    t2.Start();
+
+        //    Pay();//发送通知 
+        //    Console.ReadKey();
+        //}
+        //static void Pay()
+        //{
+        //    string tip = Console.ReadLine();
+        //    if (tip == "next")
+        //    {
+        //        autoResetEvent.Set();//收银员发送付钱通知，通过调用Set来通知客户付钱
+        //    }
+        //}
+        #endregion
+    }
+}
--- AutoResetEventApp/Properties/AssemblyInfo.cs
+++ AutoResetEventApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ConsoleApp_ITC")]
+[assembly: AssemblyTitle("AutoResetEventApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp_ITC")]
+[assembly: AssemblyProduct("AutoResetEventApp")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("999a4ea2-7b03-4970-94d8-f7820ff43b1e")]
+[assembly: Guid("9cd00c34-912a-4391-9f8f-527aacc2766e")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
