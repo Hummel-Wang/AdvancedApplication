f847ceb8f6a4755b5b61a7c945b09a3380e4ce62
Hummel-Wang <SDwangfu@126.com>
2018-12-26 15:14:37 +0800

    add AtomManipulation

---
+++ AtomManipulationApp/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AtomManipulationApp
+{
+
+    abstract class CounterBase
+    {
+        public abstract void Increment();
+        public abstract void Decrement();
+    }
+
+    class Counter : CounterBase
+    {
+        private int count;
+        public int Count => count;
+        public override void Increment()
+        {
+            count++;
+        }
+        public override void Decrement()
+        {
+            count--;
+        }
+    }
+
+    class CounterNoLock : CounterBase
+    {
+        private int count;
+        public int Count => count;
+        public override void Increment()
+        {
+            Interlocked.Increment(ref count);
+        }
+        public override void Decrement()
+        {
+            Interlocked.Decrement(ref count);
+        }
+    }
+
+
+    class Program
+    {
+        static void TestCounter(CounterBase c)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                c.Increment();
+                c.Decrement();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Incorrect counter");
+            var c1 = new Counter();
+            var t1 = new Thread(() => TestCounter(c1));
+            var t2 = new Thread(() => TestCounter(c1));
+            var t3 = new Thread(() => TestCounter(c1));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            Console.WriteLine($"Total count: {c1.Count}");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Correct  counter");
+            var c2 = new CounterNoLock();
+            t1 = new Thread(() => TestCounter(c2));
+            t2 = new Thread(() => TestCounter(c2));
+            t3 = new Thread(() => TestCounter(c2));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            Console.WriteLine($"Total count: {c2.Count}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AtomManipulationApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AtomManipulationApp2
+{
+    class Program
+    {
+        //全局变量
+        private static int _result;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Task[] _tasks = new Task[100];
+                int i = 0;
+
+                for (i = 0; i < _tasks.Length; i++)
+                {
+                    _tasks[i] = Task.Factory.StartNew((num) =>
+                   {
+                       var taskid = (int)num;
+                       Work(taskid);
+                   }, i);
+                }
+
+                Task.WaitAll(_tasks);
+                Console.WriteLine(_result);
+
+                Console.ReadKey();
+            }
+        }
+
+        //线程调用方法
+        private static void Work(int TaskID)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _result++;
+                //Interlocked.Increment(ref _result);
+            }
+        }
+    }
+}
