72b515db426b2be49d01c73916b20ada0baf93c9
Hummel-Wang <SDwangfu@126.com>
2018-12-27 09:34:41 +0800

    add Mutex&SemaphoreSlim

---
+++ MutexApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MutexApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MutexName = "Multithreading";
+            using (var m = new Mutex(false, MutexName))
+
+            {
+                // WaitOne方法的作用是阻止当前线程，直到收到其他实例释放的处理信号。
+                // 第一个参数是等待超时时间，第二个是否退出上下文同步域。
+                if (!m.WaitOne(TimeSpan.FromSeconds(10), false))
+                {
+                    Console.WriteLine("Second instance is running!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Running!");
+                    Console.ReadLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SemaphoreSlimApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreSlimApp
+{
+    class Program
+    {
+        static SemaphoreSlim semaphore = new SemaphoreSlim(4);
+        static void AccessDatabase(string name, int seconds)
+        {
+            Console.WriteLine($"{name} waits to access a database");
+            semaphore.Wait();
+            Console.WriteLine($"{name} was granted an access to a database");
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            Console.WriteLine($"{name} is completed");
+            semaphore.Release();
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                string threadName = "Thread" + i;
+                int secondsToWait = 2 + 2 * i;
+                var t = new Thread(() => AccessDatabase(threadName, secondsToWait));
+                t.Start();
+            }
+           // Console.ReadLine();
+        }
+    }
+}
