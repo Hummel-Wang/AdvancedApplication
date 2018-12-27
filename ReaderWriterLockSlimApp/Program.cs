36f4f602f931465b2250f82bd7bae8a09f3dcd08
Hummel-Wang <SDwangfu@126.com>
2018-12-27 10:27:30 +0800

    add SpinWait&ReaderWriterLockSlim

---
+++ ReaderWriterLockSlimApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReaderWriterLockSlimApp
+{
+    class Program
+    {
+        // 表示用于管理资源访问的锁定状态，可实现多线程读取或进行独占式写入访问
+        static ReaderWriterLockSlim rw = new ReaderWriterLockSlim();
+        static Dictionary<int, int> items = new Dictionary<int, int>();
+
+        static void Read()
+        {
+            Console.WriteLine("Reading contents of a dictionary");
+            while(true)
+            {
+                try
+                {
+                    // 尝试进入读取模式锁定状态
+                    rw.EnterReadLock();
+                    foreach (var key in items.Keys)
+                    {
+                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                    }
+                }
+                finally
+                {
+                    // 减少读取模式的递归计数，并在生成的计数为 0（零）时退出读取模式
+                    rw.ExitReadLock();
+                }
+            }
+        }
+
+        static void Write(string threadName)
+        {
+            while(true)
+            {
+                try
+                {
+                    int newKey = new Random().Next(250);
+                    // 尝试进入可升级模式锁定状态
+                    rw.EnterUpgradeableReadLock();
+                    if (!items.ContainsKey(newKey))
+                    {
+                        try
+                        {
+                            // 尝试进入写入模式锁定状态
+                            rw.EnterWriteLock();
+                            items[newKey] = 1;
+                            Console.WriteLine($"New key {newKey} is added to a dictionary by a {threadName}");
+                        }
+                        finally
+                        {
+                            // 减少写入模式的递归计数，并在生成的计数为 0（零）时退出写入模式
+                            rw.ExitWriteLock();
+                        }
+                    }
+                    Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                }
+                finally
+                {
+                    // 减少可升级模式的递归计数，并在生成的计数为 0（零）时退出可升级模式
+                    rw.ExitUpgradeableReadLock();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Thread(Read) { IsBackground = true }.Start();
+            new Thread(Read) { IsBackground = true }.Start();
+            new Thread(Read) { IsBackground = true }.Start();
+            new Thread(() => Write("Thread 1")) { IsBackground = true }.Start();
+            new Thread(() => Write("Thread 2")) { IsBackground = true }.Start();
+            Thread.Sleep(TimeSpan.FromSeconds(20));
+        }
+    }
+}
---
+++ SpinWaitApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpinWaitApp
+{
+    class Program
+    {
+        static volatile bool isCompleted = false;
+        static void UserModeWait()
+        {
+            while (!isCompleted)
+            {
+                Console.Write(".");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Waiting is complete");
+        }
+
+        static void HybridSpinWait()
+        {
+            // 提供对基于自旋的等待的支持
+            var w = new SpinWait();
+            while (!isCompleted)
+            {
+                // 执行单一自旋
+                w.SpinOnce();
+                // 获取对 System.Threading.SpinWait.SpinOnce 的下一次调用是否将产生处理器，同时触发强制上下文切换
+                Console.WriteLine(w.NextSpinWillYield);
+            }
+            Console.WriteLine("Waiting is complete");
+
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(UserModeWait);
+            var t2 = new Thread(HybridSpinWait);
+            Console.WriteLine("Running user mode waiting");
+            t1.Start();
+            Thread.Sleep(20);
+            isCompleted = true;
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            isCompleted = false;
+            Console.WriteLine("Running hybrid SpinWait construct waiting");
+            t2.Start();
+            Thread.Sleep(5);
+            isCompleted = true;
+        }
+    }
+}
